eacc27657af2b941b2ba4277bc972aa0710eee3e
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2022-03-30 17:00:39 -0400

    Update Paciente.cs

--- Entidades/Paciente.cs
+++ Entidades/Paciente.cs
@@ -7,19 +7,35 @@ namespace ProyectoFinal_JhonAlbert.Entidades
     {
         [Key]
         public int PacienteId { get; set; }
+
+        [Required(ErrorMessage = "Es obligatorio introducir el Nombre")]
         public string? Nombre { get; set; }
+
+        [Required(ErrorMessage = "Es obligatorio introducir el Apellido")]
         public string? Apellido { get; set; }  
+
+        [Required(ErrorMessage = "Es obligatorio introducir el Email")]
         public string? Email { get; set; }  
+
+        [Required(ErrorMessage = "Es obligatorio introducir la Direccion")]
         public string? Direccion { get; set; }
+
+        [Required(ErrorMessage = "Es obligatorio introducir el Telefono")]
         public string? Telefono { get; set; }
         public int Edad { get; set; }
 
+        [Required(ErrorMessage = "Se debe elegir un sexo")]
         // 1 Hombre  2 Mujer
         public int Sexo { get; set; }
 
+        [Required(ErrorMessage = "Se debe elegir un estado civil")]
         // 1 Soltero  2 Casado
         public int EstadoCivil { get; set; }
+
+        [Required(ErrorMessage = "Es obligatorio introducir la fecha de Nacimiento")]
         public DateTime? FechaNacimiento { get; set; }
+
+        [Required(ErrorMessage = "Es obligatorio introducir la Cedula")]
         public string? Cedula { get; set; }
 
         public Paciente()
