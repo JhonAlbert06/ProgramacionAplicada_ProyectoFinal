34b67eb6e88e58aa626c3895fd71815c39ef046c
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2022-03-30 20:50:25 -0400

    Update FacturaDetalle.cs

--- Entidades/FacturaDetalle.cs
+++ Entidades/FacturaDetalle.cs
@@ -5,8 +5,12 @@ namespace ProyectoFinal_JhonAlbert.Entidades
     public class FacturaDetalle
     {
         [Key]
+        [Range(0, int.MaxValue, ErrorMessage = "El ID debe estar en el rango de {1} y {2}.")]
         public int FacturaDetalleId { get; set; }
+        public int ProcedimientoId { get; set; }
         public string? Procedimiento { get; set; }        
+        
+        [Range(0, int.MaxValue, ErrorMessage = "El precio debe estar en el rango de {1} y {2}.")]
         public float Precio { get; set; }
 
     }
