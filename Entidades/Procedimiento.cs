900016464b5646f67556687147fe93a189e7b7ce
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2022-03-31 21:13:44 -0400

    Update Procedimiento.cs

--- Entidades/Procedimiento.cs
+++ Entidades/Procedimiento.cs
@@ -5,6 +5,7 @@ namespace ProyectoFinal_JhonAlbert.Entidades
     public class Procedimiento
     {
         [Key]
+        [Range(0, int.MaxValue, ErrorMessage = "El ID debe estar en el rango de {1} y {2}.")]
         public int ProcedimientoId { get; set; }
         
         [Required(ErrorMessage = "Es obligatorio introducir el Nombre")]
@@ -13,7 +14,7 @@ namespace ProyectoFinal_JhonAlbert.Entidades
         [Required(ErrorMessage = "Es obligatorio introducir la descripcion")]
         public string? Descripcion { get; set; }                
 
-        
+        [Range(1, int.MaxValue, ErrorMessage = "El precio debe estar en el rango de {1} y {2}.")]
         public float Precio { get; set; }
     }
 }
