cbd3b7b741c6e900d93d56ad16b77c2dcbec31ee
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2022-04-03 22:27:06 -0400

    Decima Migracion

---
+++ Migrations/20220404021755_Decima.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProyectoFinal_JhonAlbert.Migrations
+{
+    public partial class Decima : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FacturaDetalle_Factura_FacturaId",
+                table: "FacturaDetalle");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "FacturaId",
+                table: "FacturaDetalle",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FacturaDetalle_Factura_FacturaId",
+                table: "FacturaDetalle",
+                column: "FacturaId",
+                principalTable: "Factura",
+                principalColumn: "FacturaId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FacturaDetalle_Factura_FacturaId",
+                table: "FacturaDetalle");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "FacturaId",
+                table: "FacturaDetalle",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FacturaDetalle_Factura_FacturaId",
+                table: "FacturaDetalle",
+                column: "FacturaId",
+                principalTable: "Factura",
+                principalColumn: "FacturaId");
+        }
+    }
+}
