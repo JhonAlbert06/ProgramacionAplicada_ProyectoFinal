3ebad9a1927540d9bed7f2c392b45f4cc583d341
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2022-04-07 15:24:22 -0400

    Update Procedimiento.cs

--- Entidades/Procedimiento.cs
+++ Entidades/Procedimiento.cs
@@ -20,5 +20,8 @@ namespace ProyectoFinal_JhonAlbert.Entidades
 
         [Range(1, int.MaxValue, ErrorMessage = "El precio debe estar en el rango de {1} y {2}.")]
         public float Precio { get; set; }
+
+        public float TotalVendido { get; set; }
+        public int CantidadVendido { get; set; }
     }
 }
